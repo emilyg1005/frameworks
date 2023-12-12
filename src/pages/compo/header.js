dec1521f6568247084b2433897054134c007de3d
emilyg1005 <143198429+emilyg1005@users.noreply.github.com>
2023-12-12 10:18:56 -0400

    Add files via upload

---
+++ src/pages/compo/header.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/compo.module.css'
+
+export default function header() {
+    return(
+        <div id={styles.header}>
+
+        </div>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function index() {
+  return (
+    <div id={styles.main}>
+
+    </div>
+  )
+}
